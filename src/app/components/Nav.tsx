27a8a342176c644e162348533e14c42f8e716335
dharmalesmana16 <dharmalesmana16@gmail.com>
2025-07-21 09:08:28 +0800

    update link nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -84,53 +84,53 @@ export default function Nav() {
 					id="navbar-default">
 					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0   ">
 											<li className="py-2 md:py-0">
-							 <Link
+							 <a
 								href="/"
 								className=" items-center md:hidden ">
 								<img src="/image/logojbt.png" className="max-w-sm h-10 " alt="Logo Perusahaan" />
-							  </Link>
+							  </a>
 						</li>
 						<li className="py-2 md:py-0">
-							<Link
+							<a
 								href="/"
-								onClick={()=>setIsOpen(true)}
+							
 								className={"text-base text-main  rounded-sm dark:text-main  font-normal " + (path == "/" ? "bg-gray-100 py-2 px-3 font-semibold" :" hover:border-2 hover:border-gray-200 border-gray-200 hover:duration-300 py-2 px-3" )}
 								aria-current="page">
 								Home
-							</Link>
+							</a>
 						</li>
 						<li className="py-2 md:py-0">
-							<Link
+							<a
 								href="/tentangkami"
 								className={"text-base text-main  rounded-sm  " + (path == "/"  ? (bgNav ? "text-main " : "text-main ") : "text-main") + "dark:text-main  font-normal " + (path == "/tentangkami" ? "bg-gray-100 py-2 px-3 font-semibold" : "border-2 border-transparent hover:border-2 hover:border-gray-200 border-gray-200 hover:duration-300 py-2 px-3" )}>
 								Tentang
 								Kami
-							</Link>
+							</a>
 						</li>
 								<li className="py-2 md:py-0">
-							<Link
+							<a
 								href="/layanan"
 								className={"text-base text-main  rounded-sm  " + (path == "/"  ? (bgNav ? "text-main " : "text-main ") : "text-main") + "dark:text-main  font-normal " + (path == "/layanan" ? "bg-gray-100 py-2 px-3 font-semibold" : "border-2 border-transparent hover:border-2 hover:border-gray-200 border-gray-200 hover:duration-300 py-2 px-3" )}>
 								Layanan
-							</Link>
+							</a>
 						</li>
 								<li className="py-2 md:py-0">
-							<Link
+							<a
 								href="/laporantahunan"
 								className={"text-base text-main  rounded-sm " + (path == "/"  ? (bgNav ? "text-main " : "text-main ") : "text-main") + "dark:text-main  font-normal " + (path == "/laporantahunan" ? "bg-gray-100 py-2 px-3 font-semibold" : "border-2 border-transparent hover:border-2 hover:border-gray-200 border-gray-200 hover:duration-300 py-2 px-3" )}>
 								Laporan
 								Tahunan
-							</Link>
+							</a>
 						</li>
 								<li className="py-2 md:py-0">
-							<Link
+							<a
 								href="/berita"
 								
 								className={"text-base text-main  rounded-sm  " + (path == "/"  ? (bgNav ? "text-main " : "text-main ") : "text-main") +  "dark:text-main  font-normal " + (path == "/berita" ? "bg-gray-100 py-2 px-3 font-semibold" : "border-2 border-transparent hover:border-2 hover:border-gray-200 border-gray-200 hover:duration-300 py-2 px-3" )}>
 								Berita
 								&
 								Publikasi
-							</Link>
+							</a>
 						</li>
 						{/* Mobile View */}
 					

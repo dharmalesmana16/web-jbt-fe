838516b82e6b6fb66402616607f1363fc2abb5e4
dharmalesmana16 <dharmalesmana16@gmail.com>
2025-09-16 17:13:32 +0800

    updates

--- src/components/ui/TableSaham.tsx
+++ src/components/ui/TableSaham.tsx
@@ -14,10 +14,10 @@ export default function TableSaham() {
     <div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-md text-white uppercase bg-main-500 dark:bg-main-500 dark:text-gray-400">
+          <thead className="text-md text-white uppercase bg-main-500 dark:bg-main-500 dark:text-white">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Id
+                No
               </th>
               <th scope="col" className="px-6 py-3">
                 Nama

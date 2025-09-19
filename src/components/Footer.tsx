45e0edc4111104251d31f251bf85561fd05df2b0
dharma lesmana <dharmalesmana@dharmas-MacBook-Pro.local>
2025-09-19 13:54:34 +0800

    update footer,berita,direksi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
-'use client'
+"use client";
 import React from "react";
 import Link from "next/link";
-import { FaFacebook, FaInstagram, FaLocationDot, FaSquarePhone, FaTiktok, FaYoutube } from "react-icons/fa6";
+import {
+	FaFacebook,
+	FaInstagram,
+	FaLocationDot,
+	FaSquarePhone,
+	FaTiktok,
+	FaYoutube,
+} from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import Logo from "./Logo";
 export type contact = {
@@ -10,47 +17,54 @@ export type contact = {
 	icon: string;
 	deskripsi: string;
 	link?: string;
-}
+};
 export default function Footer() {
 	return (
 		<footer className="bg-[#F4F4F4] dark:bg-[#F4F4F4] shadow-lg w-[100%] top-full sticky ">
 			<div className="mx-auto  w-full p-4 ">
-				
-
 				<div className="md:flex md:justify-between container mx-auto py-20 ">
 					<div className="mb-6 md:mb-0">
 						<div className="pb-12 md:pb-20">
-						<Logo className="w-72 text-end md:h-full md:w-full"/>
+							<Logo className="w-72 text-end md:h-full md:w-full" />
 						</div>
 
 						<div className="flex w-full justify-start md:justify-end">
 							<div className="px-2">
 								<div className="border-2 border-main p-2 rounded-full hover:bg-second duration-100">
-									<a href="https://www.instagram.com/jasamargabalitol_official/" target="_blank" >
+									<a
+										href="https://www.instagram.com/jasamargabalitol_official/"
+										target="_blank"
+									>
 										<FaInstagram className="text-main text-2xl" />
 									</a>
 								</div>
 							</div>
 							<div className="px-2">
-
 								<div className="border-2 border-main p-2 rounded-full hover:bg-second duration-100">
-									<a href="https://www.tiktok.com/@jasamargabalitol?is_from_webapp=1&sender_device=pc" target="_blank" >
+									<a
+										href="https://www.tiktok.com/@jasamargabalitol?is_from_webapp=1&sender_device=pc"
+										target="_blank"
+									>
 										<FaTiktok className="text-main text-2xl" />
 									</a>
 								</div>
 							</div>
 							<div className="px-2">
-
 								<div className="border-2 border-main p-2 rounded-full hover:bg-second duration-100">
-									<a href="https://www.youtube.com/@jasamargabalitol3838" target="_blank" >
+									<a
+										href="https://www.youtube.com/@jasamargabalitol3838"
+										target="_blank"
+									>
 										<FaYoutube className="text-main text-2xl" />
 									</a>
 								</div>
 							</div>
 							<div className="px-2">
-
 								<div className="border-2 border-main p-2 rounded-full hover:bg-second duration-100">
-									<a href="https://www.facebook.com/tolbalimandara17/?locale=id_ID" target="_blank" >
+									<a
+										href="https://www.facebook.com/tolbalimandara17/?locale=id_ID"
+										target="_blank"
+									>
 										<FaFacebook className="text-main text-2xl" />
 									</a>
 								</div>
@@ -64,30 +78,31 @@ export default function Footer() {
 							</h2>
 							<ul className="text-gray-500  font-medium">
 								<li className="mb-4">
-									<Link
-										href="/"
-										className="hover:underline">
+									<Link href="/" className="hover:underline">
 										Beranda
 									</Link>
 								</li>
 								<li className="mb-4">
 									<Link
 										href="/tentangkami"
-										className="hover:underline">
+										className="hover:underline"
+									>
 										Tentang Kami
 									</Link>
 								</li>
 								<li className="mb-4">
 									<Link
 										href="/layanan"
-										className="hover:underline">
+										className="hover:underline"
+									>
 										Layanan
 									</Link>
 								</li>
 								<li>
 									<Link
 										href="/laporantahunan"
-										className="hover:underline">
+										className="hover:underline"
+									>
 										Laporan Tahunan
 									</Link>
 								</li>
@@ -99,51 +114,54 @@ export default function Footer() {
 							</h2>
 							<ul className="text-gray-500  font-medium">
 								<li className="mb-4">
-									<Link
-										href="#"
-										className="group hover:text-gray-900 text-sm tracking-light ">
-										<FaLocationDot className="text-lg inline-block group-hover:text-second duration-200" /> Jl Raya Pelabuhan Benoa No.88 Kota Denpasar, Bali 80222
-									</Link>
+									<a
+										target="_blank"
+										href="https://maps.app.goo.gl/HqV8WC5WXZoVcuij7"
+										className="group hover:text-gray-900 text-sm tracking-light "
+									>
+										<FaLocationDot className="text-lg inline-block group-hover:text-second duration-200" />{" "}
+										(Grha Bali Tol) Jl Raya Pelabuhan Benoa
+										No.88 Kota Denpasar, Bali 80222
+									</a>
 								</li>
 								<li className="mb-4">
 									<Link
 										href="#"
-										className="group hover:text-gray-900 text-sm tracking-light ">
-										<FaSquarePhone className="text-lg inline-block group-hover:text-second duration-200" /> Call Center: 14080
+										className="group hover:text-gray-900 text-sm tracking-light "
+									>
+										<FaSquarePhone className="text-lg inline-block group-hover:text-second duration-200" />{" "}
+										Call Center: 14080
 									</Link>
 								</li>
 								<li className="mb-4 ">
 									<Link
 										href="#"
-										className="group hover:text-gray-900  text-sm tracking-light ">
-										<FaSquarePhone className="text-lg inline-block group-hover:text-second duration-200 " /> Telp Kantor: (0361) 725326
+										className="group hover:text-gray-900  text-sm tracking-light "
+									>
+										<FaSquarePhone className="text-lg inline-block group-hover:text-second duration-200 " />{" "}
+										Telp Kantor: (0361) 725326
 									</Link>
 								</li>
 								<li className="mb-4">
 									<Link
 										href="#"
-										className="group hover:text-gray-900 text-sm tracking-light ">
-										<MdEmail className="text-lg inline-block group-hover:text-second duration-200 group-hover:scale-105" /> corporate@jasamargabalitol.co.id
+										className="group hover:text-gray-900 text-sm tracking-light "
+									>
+										<MdEmail className="text-lg inline-block group-hover:text-second duration-200 group-hover:scale-105" />{" "}
+										corporate@jasamargabalitol.co.id
 									</Link>
 								</li>
-
 							</ul>
 						</div>
-
 					</div>
 				</div>
 				<hr className="my-6 border-gray-300 container sm:mx-auto dark:border-gray-300 lg:my-8" />
 				<div className="container sm:flex sm:items-center sm:justify-between  mx-auto">
 					<span className="text-sm text-gray-500 sm:text-center ">
-
-						<Link
-							href="/"
-							className="hover:underline">
+						<Link href="/" className="hover:underline">
 							PT. Jasamarga Bali Tol
 						</Link>
-
 					</span>
-				
 				</div>
 			</div>
 		</footer>

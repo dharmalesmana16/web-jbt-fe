e9320ff2c507aebd2a3b883f0704f5375c01e6e1
dharmalesmana16 <dharmalesmana16@gmail.com>
2025-09-29 17:38:01 +0800

    updates

--- src/components/ui/Home/NewsInformation.tsx
+++ src/components/ui/Home/NewsInformation.tsx
@@ -8,9 +8,13 @@ import axios from 'axios'
 export default function NewsInformation() {
     const [dataBerita,setDataBerita]=useState<any>([])
     useEffect(()=>{
-        axios.get('http://202.46.152.202:8001/api/berita').then(function(resp){
-            setDataBerita(resp.data.data);
-        })
+        async function getData(){
+
+            await axios.get('http://202.46.152.202:8001/api/berita').then(function(resp){
+                setDataBerita(resp.data.data);
+            })
+        }
+        getData();
     },[])
     return (
         <div>

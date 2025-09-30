f2c76d21cf24b789c41cb970c3d84f08469e1795
dharmalesmana16 <dharmalesmana16@gmail.com>
2025-09-30 20:12:29 +0800

    cachenostore

--- src/app/(ui)/berita/page.tsx
+++ src/app/(ui)/berita/page.tsx
@@ -6,9 +6,9 @@ import Jumbotron from '@/components/ui/Berita/JumbotronBerita';
 
 export default async function page() {
   // const [dataBerita,setDataBerita] = useState<any>([])
-  const data =await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/berita`)
+  const data =await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/berita`,{ cache: 'no-store' })
   const response = await data.json();
-  console.log(response);
+  console.log(response)
   return (
     <div>
       <section className='bg-[#F8FAFF] rounded-br-[3rem]  pt-28 top-full shadow-xl relative z-10 '>
